26b8bb839965e4a06d883433de418302009f097f
Surendar M <surendar.dev@gmail.com>
2026-04-28 16:34:21 +0530

    Allow Firebase service account to be loaded from environment variable for deployment

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,21 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+let serviceAccount;
+try {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    serviceAccount = require('./firebase-service-account.json');
+  }
+} catch (error) {
+  console.error("Firebase Service Account Error:", error.message);
+}
+
+if (serviceAccount) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
 
 const db = admin.firestore();
 
